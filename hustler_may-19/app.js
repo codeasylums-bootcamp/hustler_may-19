ac5066fe83ba7e5235532d046641b4d18d8d5144
Joy Lakra <joylakra26@gmail.com>
2019-05-26 01:47:10 +0530

    First commit

---
+++ hustler_may-19/app.js
@@ -0,0 +1,19 @@
+//Railway JS file
+// apikey/x5ca0cxi5x
+function fun() { 
+/*let xhr = new XMLHttpRequest;
+            xhr.addEventListener('load', function(){
+                var response = JSON.parse(this.responseText)
+                console.log(response.train);
+                            })*/
+            var from = document.getElementById('from');
+            var to = document.getElementById('to'); 
+            var date = document.getElementById('date');
+            var clas = document.getElementById('option');
+            console.log(from.value);
+            console.log(date.value);
+            
+            /*var url= "https://api.railwayapi.com/v2/between/source/"+from+"/dest/"+to+"/date/"+date+"/apikey/x5ca0cxi5x" ;                
+            xhr.open('GET', url )
+            xhr.send();*/
+                        }  
